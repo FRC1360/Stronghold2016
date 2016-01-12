a4e276cb779389eaf24b7605814c3c4044fae924
JamieSinn <james.sinn@sinndevelopment.com>
2016-01-12 16:06:18 -0500

    Remove init.

--- src/main/java/org/usfirst/frc/team1360/robot/autonomous/groups/AutonomousExampleCommandGroup.java
+++ src/main/java/org/usfirst/frc/team1360/robot/autonomous/groups/AutonomousExampleCommandGroup.java
@@ -2,20 +2,14 @@ package org.usfirst.frc.team1360.robot.autonomous.groups;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
 import org.usfirst.frc.team1360.robot.autonomous.actions.AutonomousExampleCommand;
-import org.usfirst.frc.team1360.robot.util.CommandData;
 
 /**
  * AutonomousExampleCommandGroup - Does XXY - using XXY Commands.
  */
 public class AutonomousExampleCommandGroup extends CommandGroup
 {
-    private CommandData exampleCommandData = new CommandData();
-    private void init()
-    {
-        exampleCommandData.addBoolean(false);
-    }
     public AutonomousExampleCommandGroup()
     {
-        addSequential(new AutonomousExampleCommand(exampleCommandData));
+        addSequential(new AutonomousExampleCommand());
     }
 }
