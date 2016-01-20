c99c89c2907838f2c76a9dc8a51eb0a1751a36d5
EthChil <ethanchilderhose@gmail.com>
2016-01-19 21:01:38 -0500

    Added drivecommand and Subsystems map file.

---
+++ src/main/java/org/usfirst/frc/team1360/robot/commands/DriveCommand.java
@@ -0,0 +1,48 @@
+package org.usfirst.frc.team1360.robot.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.command.Subsystem;
+import org.usfirst.frc.team1360.robot.subsystems.DriveSubsystem;
+import org.usfirst.frc.team1360.robot.util.CommandData;
+import org.usfirst.frc.team1360.robot.util.Subsystems;
+
+public class DriveCommand extends Command
+{
+
+    public DriveCommand(CommandData data)
+    {
+
+    }
+
+    @Override
+    protected void initialize()
+    {
+        requires(Subsystems.DRIVE_SUBSYSTEM);
+    }
+
+    @Override
+    protected void execute()
+    {
+
+    }
+
+
+    @Override
+    protected boolean isFinished()
+    {
+        return false;
+    }
+
+
+    @Override
+    protected void end()
+    {
+
+    }
+
+    @Override
+    protected void interrupted()
+    {
+
+    }
+}
---
+++ src/main/java/org/usfirst/frc/team1360/robot/util/Subsystems.java
@@ -0,0 +1,12 @@
+package org.usfirst.frc.team1360.robot.util;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+import org.usfirst.frc.team1360.robot.subsystems.DriveSubsystem;
+
+public class Subsystems
+{
+    /**
+     * Drive Subsystem
+     */
+    public static final DriveSubsystem DRIVE_SUBSYSTEM = new DriveSubsystem();
+}
