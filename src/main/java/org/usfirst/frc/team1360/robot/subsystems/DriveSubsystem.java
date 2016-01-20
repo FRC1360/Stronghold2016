c1f644e2924af483a58f3548c0fbb9c187407250
EthChil <ethanchilderhose@gmail.com>
2016-01-19 20:37:26 -0500

    Added Victor declaration

--- src/main/java/org/usfirst/frc/team1360/robot/subsystems/DriveSubsystem.java
+++ src/main/java/org/usfirst/frc/team1360/robot/subsystems/DriveSubsystem.java
@@ -1,10 +1,30 @@
 package org.usfirst.frc.team1360.robot.subsystems;
 
 import edu.wpi.first.wpilibj.Talon;
+import edu.wpi.first.wpilibj.Victor;
 import edu.wpi.first.wpilibj.command.Subsystem;
+import org.usfirst.frc.team1360.robot.RobotMap;
 
 public class DriveSubsystem extends Subsystem
 {
+    /**
+     * LeftMotor Front on the Drive Subsystem
+     */
+    private final Victor DRIVE_LEFT_1 = new Victor(RobotMap.DRIVESUBSYSTEM_LEFT_1);
+    /**
+     * LeftMotor Back on the Drive Subsystem
+     */
+    private final Victor DRIVE_LEFT_2 = new Victor(RobotMap.DRIVESUBSYSTEM_LEFT_2);
+    /**
+     * RightMotor Front on the Drive Subsystem
+     */
+    private final Victor DRIVE_RIGHT_1 = new Victor(RobotMap.DRIVESUBSYSTEM_RIGHT_1);
+    /**
+     * RightMotor Back on the Drive Subsystem
+     */
+    private final Victor DRIVE_RIght_2 = new Victor(RobotMap.DRIVESUBSYSTEM_RIGHT_2);
+
+
     @Override
     protected void initDefaultCommand()
     {
