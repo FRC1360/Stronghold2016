8960f218afb6f12cdb490afbc42a8447848ff0f7
EthChil <ethanchilderhose@gmail.com>
2016-01-19 20:11:29 -0500

    Created Drive Subsystem file

---
+++ src/main/java/org/usfirst/frc/team1360/robot/subsystems/DriveSubsystem.java
@@ -0,0 +1,12 @@
+package org.usfirst.frc.team1360.robot.subsystems;
+
+import edu.wpi.first.wpilibj.Talon;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class DriveSubsystem extends Subsystem
+{
+    @Override
+    protected void initDefaultCommand()
+    {
+    }
+}
