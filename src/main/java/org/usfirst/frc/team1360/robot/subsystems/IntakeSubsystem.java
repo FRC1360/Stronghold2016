a8cbadefdc842ad81c4c39b80391934f41dfe2ac
MichGedd <michaelgeddes555@gmail.com>
2016-01-28 11:03:27 -0500

    Added new Subsystem
    Added IntakeSubsystem

---
+++ src/main/java/org/usfirst/frc/team1360/robot/subsystems/IntakeSubsystem.java
@@ -0,0 +1,17 @@
+package org.usfirst.frc.team1360.robot.subsystems;
+
+import edu.wpi.first.wpilibj.Victor;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ * Created by Michael on 2016-01-28.
+ */
+public class IntakeSubsystem extends Subsystem {
+
+    @Override
+    protected void initDefaultCommand()
+    {
+
+    }
+
+}
