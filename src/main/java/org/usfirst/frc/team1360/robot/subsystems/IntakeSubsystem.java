a4dc3e9eee1c0dc8292f3fe6c06f0192edbd4207
MichGedd <michaelgeddes555@gmail.com>
2016-01-28 11:39:10 -0500

    Added new public void
    Added IntakeBoulder() in Intake subsytem

--- src/main/java/org/usfirst/frc/team1360/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/org/usfirst/frc/team1360/robot/subsystems/IntakeSubsystem.java
@@ -2,6 +2,7 @@ package org.usfirst.frc.team1360.robot.subsystems;
 
 import edu.wpi.first.wpilibj.Victor;
 import edu.wpi.first.wpilibj.command.Subsystem;
+import org.usfirst.frc.team1360.robot.RobotMap;
 
 /**
  * Created by Michael on 2016-01-28.
@@ -12,6 +13,14 @@ public class IntakeSubsystem extends Subsystem {
     protected void initDefaultCommand()
     {
 
+    }
+
+    private final Victor INTAKE_1 = new Victor(RobotMap.INTAKE_MOTOR);
+
+
+    public void intakeBoulder(double motor)
+    {
+        INTAKE_1.set(motor);
     }
 
 }
