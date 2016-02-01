7351b3c5a373e2fee898661fe288116ac21bdf92
EthChil <ethanchilderhose@gmail.com>
2016-02-01 18:52:03 -0500

    Added Shiitake position code

--- src/org/usfirst/frc/team1360/robot/commands/ShooterCommand.java
+++ src/org/usfirst/frc/team1360/robot/commands/ShooterCommand.java
@@ -1,5 +1,8 @@
 package org.usfirst.frc.team1360.robot.commands;
 
+import org.usfirst.frc.team1360.robot.OI;
+import org.usfirst.frc.team1360.robot.util.Subsystems;
+
 import edu.wpi.first.wpilibj.command.Command;
 
 /**
@@ -7,17 +10,23 @@ import edu.wpi.first.wpilibj.command.Command;
  */
 public class ShooterCommand extends Command {
 
-    public ShooterCommand() {
+    public ShooterCommand() 
+    {
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
+    	requires(Subsystems.SHOOTER_SUBSYSTEM);
     }
 
     // Called just before this Command runs the first time
-    protected void initialize() {
+    protected void initialize() 
+    {
+    	Subsystems.SHOOTER_SUBSYSTEM.SHOOTER_ENCODER.reset();
     }
 
     // Called repeatedly when this Command is scheduled to run
-    protected void execute() {
+    protected void execute() 
+    {
+    	Subsystems.SHOOTER_SUBSYSTEM.changeShintakePosition(OI.getOperatorRightStickYAxis());
     }
 
     // Make this return true when this Command no longer needs to run execute()
--- src/org/usfirst/frc/team1360/robot/subsystems/ShooterSubsystem.java
+++ src/org/usfirst/frc/team1360/robot/subsystems/ShooterSubsystem.java
@@ -18,8 +18,11 @@ public class ShooterSubsystem extends Subsystem {
     /**
      * Shooter encoder
      */
-    Encoder SHOOTER_ENCODER = new Encoder(RobotMap.SHOOTER_ENCODER_CH1, RobotMap.SHOOTER_ENCODER_CH2, true, Encoder.EncodingType.k4X);
+    public Encoder SHOOTER_ENCODER = new Encoder(RobotMap.SHOOTER_ENCODER_CH1, RobotMap.SHOOTER_ENCODER_CH2, true, Encoder.EncodingType.k4X);
 
+    boolean isMoving = false;
+    int targetPos = 0;
+    
     /**
      * sets the motor speed
      * @param speed for the motor
@@ -28,6 +31,34 @@ public class ShooterSubsystem extends Subsystem {
     {
     	if(Math.abs(speed) < 0.1) speed = 0;
     	SHOOTER_1.set(speed);
+    }
+    
+    /**
+     * Changes the shintake position based on two presets
+     * @param Joystick from the operator that changes the shintake position
+     */
+    public void changeShintakePosition(double Joystick)
+    {
+    	if(!isMoving)
+    	{
+    		if(Joystick > 0.9)
+    		{
+    			SHOOTER_1.set(10);
+    			targetPos = 500;
+    		}
+    		if(Joystick < -0.9)
+    		{
+    			SHOOTER_1.set(-10);
+    			targetPos = 0;
+    		}
+    	}
+    	else
+    	{
+    		if(SHOOTER_ENCODER.get() > targetPos - 5 && SHOOTER_ENCODER.get() < targetPos + 5)
+    		{
+    			SHOOTER_1.set(0);
+    		}
+    	}
     }
     
     public void initDefaultCommand() {
