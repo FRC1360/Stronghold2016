1a443d151fca77bac372b7ef0bd53e26a5ae8b44
EthChil <ethanchilderhose@gmail.com>
2016-02-01 21:01:08 -0500

    added to subsystem

--- src/main/java/org/usfirst/frc/team1360/robot/util/Subsystems.java
+++ src/main/java/org/usfirst/frc/team1360/robot/util/Subsystems.java
@@ -2,6 +2,7 @@ package main.java.org.usfirst.frc.team1360.robot.util;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
 import main.java.org.usfirst.frc.team1360.robot.subsystems.DriveSubsystem;
+import main.java.org.usfirst.frc.team1360.robot.subsystems.ShooterSubsystem;
 
 public class Subsystems
 {
@@ -9,4 +10,5 @@ public class Subsystems
      * Drive Subsystem
      */
     public static final DriveSubsystem DRIVE_SUBSYSTEM = new DriveSubsystem();
+    public static final ShooterSubsystem SHOOTER_SUBSYSTEM = new ShooterSubsystem();
 }
