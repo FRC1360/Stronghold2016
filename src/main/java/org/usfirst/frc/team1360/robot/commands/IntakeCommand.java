ab9c4879620ceffd0c46cbe4294d0b9f0fb5182d
MichGedd <michaelgeddes555@gmail.com>
2016-02-03 20:51:09 -0500

    Almost finished the PIDsubsystem

--- src/main/java/org/usfirst/frc/team1360/robot/commands/IntakeCommand.java
+++ src/main/java/org/usfirst/frc/team1360/robot/commands/IntakeCommand.java
@@ -26,7 +26,7 @@ public class IntakeCommand extends Command
     // Called repeatedly when this Command is scheduled to run
     protected void execute()
     {
-    	Subsystems.INTAKE_SUBSYSTEM.intakeBoulder(0.75);
+    	Subsystems.INTAKE_SUBSYSTEM.usePIDOutput(0.75);
     }
 
     // Make this return true when this Command no longer needs to run execute()
--- src/main/java/org/usfirst/frc/team1360/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/org/usfirst/frc/team1360/robot/subsystems/IntakeSubsystem.java
@@ -1,16 +1,25 @@
 package org.usfirst.frc.team1360.robot.subsystems;
 
 import edu.wpi.first.wpilibj.Victor;
+import edu.wpi.first.wpilibj.command.PIDSubsystem;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import org.usfirst.frc.team1360.robot.RobotMap;
 
-public class IntakeSubsystem extends Subsystem 
+public class IntakeSubsystem extends PIDSubsystem
 {
 
     /**
      * Motor for intake
      */
 	private final Victor INTAKE_1 = new Victor(RobotMap.INTAKE_MOTOR);
+
+
+    public IntakeSubsystem()
+    {
+        super("IntakeSubsystem", 2.0, 0.0, 0.0);
+        setAbsoluteTolerance(0.05);
+        getPIDController().setContinuous(false);
+    }
 	
     @Override
     protected void initDefaultCommand()
@@ -29,5 +38,17 @@ public class IntakeSubsystem extends Subsystem
          */
     	if(Math.abs(motor) < 0.15) motor = 0;
         INTAKE_1.set(motor);
+    }
+
+    @Override
+    protected double returnPIDInput()
+    {
+        return 0;
+    }
+
+    @Override
+    public void usePIDOutput(double output)
+    {
+        INTAKE_1.pidWrite(output);
     }
 }
